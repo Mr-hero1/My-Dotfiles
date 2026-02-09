5eb67630109884b4ecd5d9c56b83e1b07c49e253
Mr-hero1 <204537865+Mr-hero1@users.noreply.github.com>
2026-02-09 19:59:55 +0300

    Update Config/eww dotfiles

--- Config/eww/mine/modules/workspace.sh
+++ Config/eww/mine/modules/workspace.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+if [ -z "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
+    HYPRLAND_INSTANCE_SIGNATURE=$(ls "$XDG_RUNTIME_DIR/hypr/" | head -n 1)
+    export HYPRLAND_INSTANCE_SIGNATURE
+fi
 
-# 1. Get the initial workspace ID immediately
-hyprctl activeworkspace -j | jq -r '.id'
+hyprctl monitors -j | jq -r '.[] | select(.focused) | .activeWorkspace.id'
 
-# 2. Listen for events and filter for workspace changes
-# 'stdbuf -o0' ensures the output isn't buffered (so it's instant)
-# 'uniq' prevents the double-triggering you're seeing
-socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | \
-  stdbuf -o0 awk -F '>>' '/workspace>>/ {print $2}' | \
+socat -u UNIX-CONNECT:"$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" - | \
+  stdbuf -o0 awk -F '>>|,' '/^workspacev2>>/ {print $2}' | \
   stdbuf -o0 uniq
