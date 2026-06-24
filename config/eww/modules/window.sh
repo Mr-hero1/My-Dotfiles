e7625c00d84ebefebda8b74da1cc19d1953908e4
Hero¹ <204537865+Mr-hero1@users.noreply.github.com>
2026-06-25 02:04:18 +0300

    Add files via upload

--- config/eww/modules/window.sh
+++ config/eww/modules/window.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+
 get_title() {
-niri msg -j focused-window | jq -r '.title // "~"' 2>/dev/null | awk '{ if (length($0) > 100) print substr($0, 1, 97) "..."; else print $0; }'
+    niri msg -j focused-window 2>/dev/null | jq -r '
+        .title // "~" | 
+        if length > 90 then .[0:87] + "..." else . end
+    '
 }
 
 get_title
 
-niri msg -j event-stream | jq --unbuffered -c 'select(.WindowFocusChanged or .WindowTitleChanged)' | while read -r _; do
+niri msg -j event-stream | grep --line-buffered -E 'WindowFocusChanged|WindowTitleChanged' | while read -r _; do
     get_title
 done
